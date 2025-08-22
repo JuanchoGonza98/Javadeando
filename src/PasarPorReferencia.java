868a51e0966956a8af4ef4f809fbb09ee39adfbc
Juancho <jhonandres90@hotmail.com>
2025-08-22 10:13:05 -0300

    Se agrega paso por referencia a Arrays.

---
+++ src/PasarPorReferencia.java
@@ -0,0 +1,32 @@
+public class PasarPorReferencia {
+
+    public static void main(String[] args) {
+        int[] edad = {10 , 11, 12};
+
+        System.out.println("Iniciamos el metodo main");
+        for(int i = 0; i < edad.length; i++){
+            System.out.println("edad[i] = " + edad[i] );
+        }
+        System.out.println("Despues de llama al metodo test");
+        test(edad);
+        System.out.println("Despues de llamar al metodo test");
+        for(int i = 0; i < edad.length; i++)
+        {
+            System.out.println("edad[i] = " + edad[i] );
+        }
+
+        System.out.println("Finaliza el metodo main con los datos del arreglo modificados!");
+    }
+
+public static void test(int[] edadArr)
+{
+    System.out.println("Inicia el metodo test");
+    for(int i = 0; i < edadArr.length; i++){
+        edadArr[i] = edadArr[i] + 20;
+    }
+    System.out.println("Finaliza el metodo test");
+}
+}
+
+
+
